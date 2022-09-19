b1a69a32b77507bd39b6d080aa20db414297dcf6
Dj Lukis.LT <lukis101@gmail.com>
2022-09-19 19:00:13 +0300

    Mesh Tools: bounds copy/paste, subgroup for asset saving

--- Scripts/Editor/MeshAssetSaver.cs
+++ Scripts/Editor/MeshAssetSaver.cs
@@ -6,27 +6,35 @@ namespace DJL
 	public static class MeshAssetSaver
 	{
 		[MenuItem("CONTEXT/MeshFilter/Export as asset")]
-		public static void SaveMeshInPlace (MenuCommand menuCommand) {
+		public static void SaveMeshInPlace(MenuCommand menuCommand) {
 			MeshFilter mf = menuCommand.context as MeshFilter;
 			Mesh m = mf.sharedMesh;
 			SaveMesh(m, m.name, true, false);
 		}
 
 		[MenuItem("CONTEXT/MeshFilter/Optimise and export as asset")]
-		public static void SaveMeshNewInstanceItem (MenuCommand menuCommand) {
+		public static void SaveMeshNewInstanceItem(MenuCommand menuCommand) {
 			MeshFilter mf = menuCommand.context as MeshFilter;
 			Mesh m = mf.sharedMesh;
 			SaveMesh(m, m.name, true, true);
 		}
 
-		[MenuItem("CONTEXT/SkinnedMeshRenderer/Export as asset")]
-		public static void SaveSkinnedMeshInPlace (MenuCommand menuCommand) {
+		[MenuItem("CONTEXT/SkinnedMeshRenderer/Export as asset/Regular")]
+		public static void SaveSkinnedMeshInPlace(MenuCommand menuCommand) {
 			SkinnedMeshRenderer smr = menuCommand.context as SkinnedMeshRenderer;
 			Mesh m = smr.sharedMesh;
 			SaveMesh(m, m.name, true, false);
 		}
-		[MenuItem("CONTEXT/SkinnedMeshRenderer/Export as asset with bounds")]
-		public static void SaveSkinnedMeshInPlaceBounds (MenuCommand menuCommand) {
+
+		[MenuItem("CONTEXT/SkinnedMeshRenderer/Export as asset/Optimized")]
+		public static void SaveSkinnedMeshNewInstanceItem(MenuCommand menuCommand) {
+			SkinnedMeshRenderer smr = menuCommand.context as SkinnedMeshRenderer;
+			Mesh m = smr.sharedMesh;
+			SaveMesh(m, m.name, true, true);
+		}
+
+		[MenuItem("CONTEXT/SkinnedMeshRenderer/Export as asset/With custom bounds")]
+		public static void SaveSkinnedMeshInPlaceBounds(MenuCommand menuCommand) {
 			SkinnedMeshRenderer smr = menuCommand.context as SkinnedMeshRenderer;
 			Mesh m = smr.sharedMesh;
 			//Bounds bounds = m.bounds
@@ -34,11 +42,42 @@ namespace DJL
 			SaveMesh(m, m.name, true, false);
 		}
 
-		[MenuItem("CONTEXT/SkinnedMeshRenderer/Optimise and export as asset")]
-		public static void SaveSkinnedMeshNewInstanceItem (MenuCommand menuCommand) {
+		// Bounds copy-pasting
+		static bool boundscopied = false;
+		static Bounds copiedbounds = new Bounds();
+		[MenuItem("CONTEXT/SkinnedMeshRenderer/Copy Bounds")]
+		public static void CopySkinnedMeshBounds(MenuCommand menuCommand)
+		{
 			SkinnedMeshRenderer smr = menuCommand.context as SkinnedMeshRenderer;
-			Mesh m = smr.sharedMesh;
-			SaveMesh(m, m.name, true, true);
+			copiedbounds = smr.localBounds;
+			boundscopied = true;
+		}
+		[MenuItem("CONTEXT/MeshRenderer/Copy Bounds")]
+		public static void CopyMeshRendererBounds(MenuCommand menuCommand)
+		{
+			MeshRenderer mr = menuCommand.context as MeshRenderer;
+			copiedbounds = mr.bounds;
+			boundscopied = true;
+		}
+		[MenuItem("CONTEXT/Mesh/Copy Bounds")]
+		public static void CopyMeshBounds(MenuCommand menuCommand)
+		{
+			Mesh smr = menuCommand.context as Mesh;
+			copiedbounds = smr.bounds;
+			boundscopied = true;
+		}
+
+		[MenuItem("CONTEXT/SkinnedMeshRenderer/Paste Bounds", true)]
+		public static bool CanPasteSkinnedMeshBounds(MenuCommand menuCommand)
+		{
+			return boundscopied;
+		}
+		[MenuItem("CONTEXT/SkinnedMeshRenderer/Paste Bounds", false)]
+		public static void PasteSkinnedMeshBounds(MenuCommand menuCommand)
+		{
+			SkinnedMeshRenderer smr = menuCommand.context as SkinnedMeshRenderer;
+			Undo.RecordObject(smr, "Paste Bounds");
+			smr.localBounds = copiedbounds;
 		}
 
 		public static void SaveMesh (Mesh mesh, string name, bool makeNewInstance, bool optimizeMesh) {
